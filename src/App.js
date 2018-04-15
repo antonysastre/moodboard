202189a776641d973216b3babf357ef2cc9f1cda
Antony Sastre <antony@thenewconcept.com>
2018-04-15 14:02:14 +0200

    Board preview

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
 import React, { Component } from 'react'
 import backend from './backend.js'
+import BoardPreview from './BoardPreview.js'
 import './App.css'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { messages: []}
+    this.state = { boards: []}
   }
 
   componentWillMount() {
-    let messagesRef = backend.database()
-      .ref('messages')
+    let boardRef = backend.database()
+      .ref('boards')
       .orderByKey()
       .limitToLast(100)
 
-    messagesRef.on('child_added', snapshot => {
-      let message = { text: snapshot.val(), id: snapshot.key }
-      this.setState({ messages: [message].concat(this.state.messages) })
+    boardRef.on('child_added', snapshot => {
+      let board  = { src: snapshot.val(), id: snapshot.key }
+      let boards = [board].concat(this.state.boards)
+      this.setState({ boards })
+    })
+
+    boardRef.on('child_removed', snapshot => {
+      let boards = this.state.boards.filter(b => b.id !== snapshot.key)
+      this.setState({ boards })
     })
   }
 
   // private
 
-  addMessage(e) {
-    e.preventDefault()
-    backend.database().ref('messages').push( this.inputEl.value )
+  deleteBoard(id) {
+    backend.database().ref('boards').child(id).remove();
+  }
+
+  createBoard(e) {
+    e.preventDefault();
+    backend.database().ref('boards').push( this.inputEl.value )
     this.inputEl.value = ''
   }
 
   render() {
     return (
-      <form onSubmit={this.addMessage.bind(this)}>
-        <ul>
+      <form onSubmit={this.createBoard.bind(this)}>
+        <div className="board-list">
           {
-            this.state.messages.map( message => <li key={message.id}>{message.text}</li> )
+            this.state.boards.map( board => <BoardPreview
+              id={board.id}
+              key={board.id}
+              src={board.src}
+              deleteBoard={this.deleteBoard}
+            /> )
           }
-        </ul>
+        </div>
+        <hr/>
         <input type="text" ref={ el => this.inputEl = el }/>
         <input type="submit"/>
       </form>
